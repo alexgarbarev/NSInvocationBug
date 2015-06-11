9c78ffd61cc2bf6e1d8aa309eb59c775cd31379c
Aleksey Garbarev <alex.garbarev@gmail.com>
2015-06-12 03:27:29 +0600

    Added test #4

--- NSInvocationBug/ViewController.m
+++ NSInvocationBug/ViewController.m
@@ -27,6 +27,7 @@ typedef struct {
     [self callTestMethod1];
     [self callTestMethod2];
     [self callTestMethod3];
+    [self callTestMethod4];
 
 }
 
@@ -239,6 +240,58 @@ typedef struct {
     NSLog(@"------------------------");
     
 }
+
+//-------------------------------------------------------------------------------------------
+#pragma mark - Test 4
+//-------------------------------------------------------------------------------------------
+
+
+- (void)callTestMethod4
+{
+    SEL selector = @selector(test4MethodWithDouble1:double2:double3:double4:double5:doublesStruct:);
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[self methodSignatureForSelector:selector]];
+    invocation.selector = selector;
+    invocation.target = self;
+    
+    Float64 value = 1;
+    [invocation setArgument:&value atIndex:2];
+    value = 2;
+    [invocation setArgument:&value atIndex:3];
+    value = 3;
+    [invocation setArgument:&value atIndex:4];
+    value = 4;
+    [invocation setArgument:&value atIndex:5];
+    value = 5;
+    [invocation setArgument:&value atIndex:6];
+    
+    UIEdgeInsets structure =  UIEdgeInsetsMake(10, 20, 30, 40);
+    [invocation setArgument:&structure atIndex:7];
+    
+    
+    [invocation invoke];
+}
+
+- (void)test4MethodWithDouble1:(Float64)double1
+                       double2:(Float64)double2
+                       double3:(Float64)double3
+                       double4:(Float64)double4
+                       double5:(Float64)double5
+                 doublesStruct:(UIEdgeInsets)doublesStruct
+{
+    NSLog(@"-------- TEST4 ---------");
+    NSLog(@"1: %g", double1);
+    NSLog(@"2: %g", double2);
+    NSLog(@"3: %g", double3);
+    NSLog(@"4: %g", double4);
+    NSLog(@"5: %g", double5);
+    
+    NSLog(@"Structure = %@", NSStringFromUIEdgeInsets(doublesStruct));
+    
+    
+    NSLog(@"------------------------");
+    
+}
+
 
 
 @end
